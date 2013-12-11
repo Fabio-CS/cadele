6022e8c6cd4b26ccdcfb2360bb5f56cfbd10e55a
Fabio Miranda <fmiranda@localhost>
2013-12-11 19:02:16 -0200

    Telas finalizadas

--- src/br/com/winget/cadele/MainActivity.java
+++ src/br/com/winget/cadele/MainActivity.java
@@ -1,5 +1,8 @@
 package br.com.winget.cadele;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -9,7 +12,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.login);
 	}
 
 	@Override
@@ -17,6 +20,10 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void getUserById() throws URISyntaxException{
+		URI url = new URI("http://10.0.2.2:8080/WebserviceCadele/webresources/user/?id=1");
 	}
 
 }
